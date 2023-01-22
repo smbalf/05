899aa6cc175f015c13c0456c1127a40ec8e4a6b6
Baudrillard <smbalf+222@gmail.com>
2023-01-22 18:07:30 +0000

    successful identification on minimap
    identify trees correctly on minimap

--- minimap_bot.py
+++ minimap_bot.py
@@ -19,17 +19,19 @@ TREE_IMAGE = "images/minimap_tree.png"
 
 app = Application().connect(title=TITLE)
 app.top_window().set_focus()
+
 minimap = pyautogui.screenshot(region=REGION)
 minimap.save("output_images/current_minimap.png")
 
-
 minimap = np.array(minimap)
 os.system('cls')
+
 tree_img = cv2.imread(TREE_IMAGE)
 result = cv2.matchTemplate(minimap, tree_img, cv2.TM_CCOEFF_NORMED)
-threshold = 0.65
 
+threshold = 0.43
 loc = np.where(result >= threshold)
+
 trees_found = loc[0].size
 if trees_found > 0:
     print(f'Number of trees found: {trees_found}')
@@ -37,8 +39,8 @@ if trees_found > 0:
         for i in range(loc[0].size):
             x = loc[1][i]
             y = loc[0][i]
-            print(f"Tree found at: ({x}, {y})")
             cv2.putText(minimap, ".", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (125, 246, 55), 2)
             cv2.imwrite(f'output_images/trees_found.png', minimap)
 else:
     print("No trees found.")
+    cv2.imwrite(f'output_images/trees_found.png', minimap)
