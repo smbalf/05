5b5edeed5195f13087a28313922a9ff4d82dfc2a
Baudrillard <smbalf+222@gmail.com>
2023-01-22 18:39:46 +0000

    implemented greyscaling
    now with 80% confidence can find the right trees on the minimap...

--- minimap_bot.py
+++ minimap_bot.py
@@ -1,9 +1,10 @@
+import os
+from dotenv import load_dotenv
 import pyautogui
 from pywinauto import Application
 import cv2
 import numpy as np
-import os
-from dotenv import load_dotenv
+
 
 load_dotenv()
 
@@ -22,25 +23,30 @@ app.top_window().set_focus()
 
 minimap = pyautogui.screenshot(region=REGION)
 minimap.save("output_images/current_minimap.png")
-
 minimap = np.array(minimap)
-os.system('cls')
+
+grey_minimap = cv2.cvtColor(minimap, cv2.COLOR_RGB2GRAY)
 
 tree_img = cv2.imread(TREE_IMAGE)
-result = cv2.matchTemplate(minimap, tree_img, cv2.TM_CCOEFF_NORMED)
+grey_tree_img = cv2.cvtColor(tree_img, cv2.COLOR_BGR2GRAY)
 
-threshold = 0.43
-loc = np.where(result >= threshold)
+grey_result = cv2.matchTemplate(grey_minimap, grey_tree_img, cv2.TM_CCOEFF_NORMED)
 
-trees_found = loc[0].size
-if trees_found > 0:
-    print(f'Number of trees found: {trees_found}')
-    if trees_found <= 30:
-        for i in range(loc[0].size):
-            x = loc[1][i]
-            y = loc[0][i]
-            cv2.putText(minimap, ".", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (125, 246, 55), 2)
-            cv2.imwrite(f'output_images/trees_found.png', minimap)
+
+os.system('cls')
+
+grey_threshold = 0.8
+grey_loc = np.where(grey_result >= grey_threshold)
+grey_trees_found = grey_loc[0].size
+if grey_trees_found > 0:
+    print(f'Number of trees found: {grey_trees_found}')
+    if grey_trees_found <= 30:
+        for i in range(grey_loc[0].size):
+            x = grey_loc[1][i]
+            y = grey_loc[0][i]
+            cv2.putText(grey_minimap, ".", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (125, 246, 55), 2)
+            cv2.imwrite(f'output_images/grey_trees_found.png', grey_minimap)
+
 else:
     print("No trees found.")
-    cv2.imwrite(f'output_images/trees_found.png', minimap)
+    cv2.imwrite(f'output_images/grey_trees_found.png', grey_minimap)
